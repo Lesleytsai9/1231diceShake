e0bb2d9f04d31958c1a1eec9a407077a2d0d10cc
Lesleytsai9 <e861k91677@gmail.com>
2019-12-31 01:54:49 +0800

    Initial Commit

---
+++ 1231diceShake/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  1231diceShake
+//
+//  Created by 蔡家雯 on 2019/12/31.
+//  Copyright © 2019 lesley tsai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
